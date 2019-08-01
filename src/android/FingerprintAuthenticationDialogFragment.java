3d252d5c8e075810fce898ba51c523353e1b03af
Sander Keymeulen <sander.keymeulen@hotmail.com>
2019-08-01 14:10:07 +0200

    import of resources is needed

--- src/android/FingerprintAuthenticationDialogFragment.java
+++ src/android/FingerprintAuthenticationDialogFragment.java
@@ -23,6 +23,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.Resources;
 import android.hardware.fingerprint.FingerprintManager;
 import android.os.Bundle;
 import android.util.Log;
